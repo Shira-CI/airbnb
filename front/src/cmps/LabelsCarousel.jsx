57a20e11ff2c340c5d02e06344863470feb59670
Shira Cohen <shira140@gmail.com>
2023-10-18 17:45:09 +0300

    labels carousel arrows functionality

--- front/src/cmps/LabelsCarousel.jsx
+++ front/src/cmps/LabelsCarousel.jsx
@@ -5,44 +5,48 @@ import { filterService } from "../services/filter.service"
 export function LabelsCarousel() {
     const [isDragging, setIsDragging] = useState(false)
     const categories = filterService.getCategories()
-    console.log(categories)
     const carouselRef = useRef(null)
     const arrowLeftRef = useRef(null)
     const arrowRightRef = useRef(null)
     const categoryRef = useRef(null)
     const savedIsDragging = useRef(false)
 
-    let carouselElement, startX, startScrollLeft, categoryElement, firstCategoryWidth, movingWidth
-
+    let carouselElement, startX, startScrollLeft, movingWidth
 
     useEffect(() => {
         savedIsDragging.current = isDragging
     }, [isDragging])
 
+    useEffect(() => {
+        carouselElement = carouselRef.current
+        if (!carouselElement) return
 
-    // useEffect(() => {
-    //     categoryElement = categoryRef.current
-    //     if (!categoryElement) return
-    //     firstCategoryWidth = categoryElement.offsetWidth
-    // }, [])
-
+        function handleScroll() {
+            if (carouselElement.scrollLeft === 0) {
+                arrowLeftRef.current.style.display = 'none'
+                arrowRightRef.current.style.display = 'block'
+            } else if (carouselElement.scrollLeft + carouselElement.clientWidth >= carouselElement.scrollWidth) {
+                arrowLeftRef.current.style.display = 'block'
+                arrowRightRef.current.style.display = 'none'
+            } else {
+                arrowLeftRef.current.style.display = 'block'
+                arrowRightRef.current.style.display = 'block'
+            }
+        }
+        carouselElement.addEventListener('scroll', handleScroll)
+    }, [])
 
     useEffect(() => {
+        arrowLeftRef.current.style.display = 'none'
         const arrowsElements = {
             arrowLeftElement: arrowLeftRef.current,
             arrowRightElement: arrowRightRef.current
         }
 
-
         for (const key in arrowsElements) {
             const arrowElement = arrowsElements[key]
             arrowElement.addEventListener('click', () => {
-                // checkArrowVisibility()
-                // console.log(arrowElement.id)
-                // console.log(carouselElement.offsetWidth)
-                // console.log(carouselElement.scrollLeft)
                 movingWidth = carouselElement.offsetWidth - 216
-                // console.log(movingWidth)
                 carouselElement.scrollLeft += arrowElement.id === 'left' ? -movingWidth : movingWidth
             })
         }
@@ -51,7 +55,6 @@ export function LabelsCarousel() {
 
 
     useEffect(() => {
-        carouselElement = carouselRef.current
         if (!carouselElement) return
         carouselElement.addEventListener('mousedown', dragStart)
         carouselElement.addEventListener('mousemove', dragging)
@@ -62,41 +65,25 @@ export function LabelsCarousel() {
             carouselElement.removeEventListener('mousemove', dragging)
             document.removeEventListener('mouseup', dragStop)
         }
-
     }, [])
 
-
-    function checkArrowVisibility() {
-        if (carouselElement.scrollLeft === 0) {
-            arrowLeftRef.current.style.display = 'none'
-        } else {
-            arrowLeftRef.current.style.display = 'block'
-        }
-    }
 
     function dragStart(e) {
         if (!carouselElement) return
         setIsDragging(true)
-        // console.log('dragstart')
         carouselElement.classList.add('dragging')
         startX = e.pageX
         startScrollLeft = carouselElement.scrollLeft
     }
     function dragging(e) {
         if (!savedIsDragging.current) return
-
         carouselElement.scrollLeft = startScrollLeft - (e.pageX - startX)
     }
     function dragStop() {
-        // console.log('dragstop')
         setIsDragging(false)
-        // console.log(isDragging)
         carouselElement.classList.remove('dragging')
     }
 
-
-
-    // console.log(categories)
     if (!categories) return
     return (
         <div className="wrapper">
@@ -104,28 +91,22 @@ export function LabelsCarousel() {
             <ul className="labelsCarousel" ref={carouselRef}>
                 {categories.map((category, idx) => {
 
-                        return (<li className="category" key={idx} ref={categoryRef}>
-                            <div className="category-img">
-                                <img src={require(`../assets/img/categories/${category.url}.png`)}
-                                    alt={category.url}
-                                //    { drragable="false"} 
-                                />
-                            </div>
+                    return (<li className="category" key={idx} ref={categoryRef}>
+                        <div className="category-img">
+                            <img src={require(`../assets/img/categories/${category.url}.png`)}
+                                alt={category.url}
+                            />
+                        </div>
 
-                            <div className='category-name'>
-                                <span>
-
-                                    {category.name}
-
-                                </span>
-                            </div>
-
-                        </li>)
-                    }
-                
+                        <div className='category-name'>
+                            <span>
+                                {category.name}
+                            </span>
+                        </div>
+                    </li>)
+                }
                 )}
             </ul>
-
 
             <i className="material-symbols-outlined arrow-right" ref={arrowRightRef} id="right">arrow_forward_ios</i>
         </div>
