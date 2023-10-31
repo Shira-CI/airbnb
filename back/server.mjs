62ffdc4e67101b1e6f9d16d3935093514d35adf5
Shira Cohen <shira140@gmail.com>
2023-10-31 13:31:17 +0200

    server fix for render

--- back/server.mjs
+++ back/server.mjs
@@ -12,7 +12,7 @@ app.use(cookieParser())
 app.use(express.json())
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, 'public')))
+  app.use(express.static(path.resolve( 'public')))
 } else {
   const corsOptions = {
     origin: [
@@ -53,7 +53,7 @@ setupSocketAPI(server)
 const port = process.env.PORT || 3030
 
 app.get('/**', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    res.sendFile(path.join( 'public', 'index.html'))
 })
 
 server.listen(port, () => {
