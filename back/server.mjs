5cc7203c5cff21f617ecd6678afc7ca1ef654f2e
Shira Cohen <shira140@gmail.com>
2023-10-31 12:43:26 +0200

    server adjusment for render

--- back/server.mjs
+++ back/server.mjs
@@ -12,7 +12,7 @@ app.use(cookieParser())
 app.use(express.json())
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve('public')))
+  app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
   const corsOptions = {
     origin: [
